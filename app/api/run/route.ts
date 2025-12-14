693a6fdc1f8c3fc94a4f58f7b2fed56f656cbbe2
Samius Sazin <samiussazin45@gmail.com>
2025-12-15 01:19:26 +0600

    fix

--- app/api/run/route.ts
+++ app/api/run/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { runCode } from '../../../compiler/main';
+import { runCode } from '@/compiler/main';
 
 export async function POST(request: NextRequest) {
   try {
